c661a5185d8fec47b2665a4f373d67b39ac87d7c
Soulaimane Ouhmida <105684836+Soulaimane07@users.noreply.github.com>
2024-08-18 18:44:49 +0100

    99

--- front/src/Pages/Admin/Home/Home.js
+++ front/src/Pages/Admin/Home/Home.js
@@ -28,9 +28,9 @@ const cards = [
         <div className='min-h-screen px-20 py-16 bg-gray-50 text-gray-700'>
           <div className="grid gap-6 mb-8  md:mb-12 md:grid-cols-2">
               {cards?.map((item,key)=>(
-              <Link to={item.link} key={key} className="flex flex-col shadow-md rounded-md p-8 bg-white text-gray-700 border-2 border-opacity-5 border-gray-600">
-                  <h2 className='text-xl font-semibold '> {item.title} ({item.number ?? 0}) </h2> 
-                  <Link to={item.link} className=' mt-4 hover:text-orange-500 transition-all'> Read more... </Link>  
+              <Link to={item.link} key={key} className="flex flex-col hover:text-orange-500 shadow-md rounded-md p-8 bg-white text-gray-700 border-2 border-opacity-5 border-gray-600">
+                  <h2 className='text-xl font-semibold  transition-all'> {item.title} ({item.number ?? 0}) </h2> 
+                  <Link to={item.link} className=' mt-4 text-gray-700 hover:text-orange-500 transition-all'> Read more... </Link>  
               </Link>
               ))}
           </div>
